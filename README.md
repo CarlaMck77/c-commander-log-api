db9246fa323bd7b3b5eed4b4b09a1aa6d07c1848
Carla McKenzie <carlamckenzie@Carlas-MacBook-Pro.local>
2020-11-28 22:27:00 -0500

    added service to start up

---
+++ README.md
@@ -0,0 +1,1 @@
+Endpoint http://localhost:5000/api/commands
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace commander_log_api
         {
 
             services.AddControllers();
+            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "commander_log_api", Version = "v1" });
